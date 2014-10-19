ac4b6e3fd9d8bede9b879581acf09e5481fd7146
Brad Johnson <info@bradleyjohnsonphotography.com>
2014-10-19 16:09:56 -0400

    Added form output and inut views, controller and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
 	get 'about', to: 'pages#about' 
 
+	get 'form_input',   to: 'form_example#form_input'
+	
+	get 'form_output',  to: 'form_example#form_output'
+
 	resources :users
 
 end
