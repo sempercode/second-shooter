1e81dc78fb9cc845d098f27f3301d3e4bd84fed5
Brad Johnson <info@bradleyjohnsonphotography.com>
2014-11-11 20:52:57 -0500

    Fix blog to_s by removing stupid equals sign.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106232817) do
+ActiveRecord::Schema.define(version: 20141112014558) do
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "content_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["content_id"], name: "index_comments_on_content_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
