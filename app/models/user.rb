a4b79aac7994c6e55d9de3da27fde055a9d82a46
Brad Johnson <info@bradleyjohnsonphotography.com>
2014-11-11 20:11:14 -0500

    More minor blog (heroku) fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
- validates :first_name, :presence => true
- validates :last_name, :presence => true
- validates :email, :presence => true
- validates :location, :presence => true
- validates :website, :presence => true
- validates :hourly_rate, :presence => true
+ validates :first_name, presence: true
+ validates :last_name, presence: true
+ validates :email, presence: true
+ validates :location, presence: true
+ validates :website, presence: true
+ validates :hourly_rate, presence: true
 end
